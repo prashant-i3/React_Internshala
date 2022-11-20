dbcc81e3ca0581253696cf0cc1f281b5eb60bfcc
Prashant Pandey <pandeyprashant718@gmail.com>
2022-11-20 10:39:36 +0530

    Winner Calculation - State Sharing Across Components

--- React/_project/tictactoe/src/components/Board.jsx
+++ React/_project/tictactoe/src/components/Board.jsx
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
+const Board = ( {board, handleSquareClick}) => {
   /*
     useState return an array of size 2-element
       first=> return the state itself (i.e., 'board')
       second=> return the update function (i.e., 'setBoard')
   */
+/*
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [isXNext, setIsXNext] = useState(false);
 
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
 
+  // // function for logic of onclick event
+  // const handleSquareClick = (position) => {
 
-  // function for logic of onclick event
-  const handleSquareClick = (position) => {
-
-    if( board[position] ){
-      return;
-    }
+  //   if( board[position] ){
+  //     return;
+  //   }
 
 
     
-    //callback function
-    setBoard( (prev) => {
+  //   //callback function
+  //   setBoard( (prev) => {
 
-      return prev.map((square, pos) => {
-        if(pos === position){
-          return isXNext ? 'X' : 'O';
-        }
+  //     return prev.map((square, pos) => {
+  //       if(pos === position){
+  //         return isXNext ? 'X' : 'O';
+  //       }
 
-        return square;
+  //       return square;
 
-      })
+  //     })
 
-    })
+  //   })
 
-    setIsXNext( (prev) => !prev );
+  //   setIsXNext( (prev) => !prev );
     
-  }
-
+  // }
+*/
   // this function helps to reder buttons
   const renderSquare = position => {
     return(
